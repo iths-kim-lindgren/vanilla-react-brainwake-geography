869f8b95b72987e59271baefd52f30323c051eef
Kim <kim.z.lindgren@gmail.com>
2020-09-04 14:10:32 +0200

    fredag em

---
+++ src/components/Map.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const Map = props => {
+
+
+    // const script = document.createElement("script")
+
+    //     script.src="../node_modules/d3/dist/d3.min.js"
+    //     script.src="https://unpkg.com/topojson@3"
+    //     script.src="d3/geo.js"
+
+
+
+    return (
+        <div>
+            <h2></h2>
+            <h3></h3>
+            <div id="map">Hej och hallå</div>
+        </div>
+    )
+}
+
+export default Map;
---
+++ src/components/StartingScreen.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const StartingScreen = props => {
+    
+    return (
+        <section>
+            <article>
+                <button>Select a country</button>
+            </article>
+            <article>
+                <button>Select a country</button>
+            </article>
+            <article>
+                <button>Begin</button>
+            </article>
+        </section>
+    )
+}
+
+export default StartingScreen;
